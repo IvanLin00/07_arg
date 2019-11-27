a2d91c77054d12de44adcf94301a2b1826c63b60
Ivan Lin <ilin00@stuy.edu>
2019-11-26 21:39:22 -0500

    fin i think

--- arg.c
+++ arg.c
@@ -1,23 +1,22 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-char ** parse_args(char *line){ //something run in function
-  char *s1 = line;
-  char ** return_array;
-  for(int i = 0; i < 6; i+=1){
-    return_array[i] = strsep(&s1, " ");
-    if (s1 == NULL) break;
-  }
-  return return_array;
+char ** parse_args(char *line){
+	char ** return_array = malloc(5 * sizeof(char *));
+	char *curr = line;
+	int i = 0;
+	while (curr){
+		return_array[i] = strsep(&curr, " ");
+		i++;
+	}
+	return return_array;
 }
 
 int main(){
-  char line [100] = "ls -a -l";
-  char ** args = parse_args(line);
-  // printf("%s\n",args[0]);
-  // printf("%s\n",args[1]);
-  // printf("%s\n",args[2]);
-  //execvp(args[0], args);
-  return 0;
+	char line[100] = "ls -a -l";
+	char ** args = parse_args(line);
+	execvp(args[0], args);
+	return 0;
 }
